172f0d901763be162cb4e8bbf7d96441b458c63f
immanuelcherui1 <mmanuelcherui1@gmail.com>
2024-03-09 10:54:15 +0300

    Unable to count

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value=''):
+        if not isinstance(value, str) or value == '':
+            print('The value must be a string.')
+        else:
+            self.value = value
+
+    def is_sentence(self):
+        return self.value.endswith('.')
+
+    def is_question(self):
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        sentences = 0
+        for word in self.value.split():
+            if word.endswith('.') or word.endswith('?') or word.endswith('!'):
+                sentences += 1
+        return sentences
